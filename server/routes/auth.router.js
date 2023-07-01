377e34a5b2c10381a7f1f2ff5a73d343c408c060
Heorhii Sanchenko <jorhesancho@gmail.com>
2023-07-01 13:14:52 +0300

    Remove compiled js files

--- server/routes/auth.router.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const authController = require('../controllers/auth.controller');
-const router = express.Router();
-router.post('/login', authController.login);
-router.post('/register', authController.register);
-module.exports = router;
