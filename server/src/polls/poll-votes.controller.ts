20f223329fe5449d60d87ff38e9814810c7908ff
Heorhii Sanchenko <jorhesancho@gmail.com>
2023-07-16 19:45:10 +0300

    feat(polls): BE count poll votes

--- server/src/polls/poll-votes.controller.ts
+++ server/src/polls/poll-votes.controller.ts
@@ -1,14 +1,18 @@
-import { Body, Controller, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { VotesMutationService } from './votes/votes-mutation.service';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { CreateVotesDto } from './dto/create-votes/create-votes.dto';
 import { RequestUser } from 'src/users/decorators/user.decorator';
 import { UserEntity } from 'src/users/entity/user.entity';
+import { VotesQueryService } from './votes/votes-query.service';
 
 @ApiTags('Polls')
 @Controller('polls')
 export class PollVotesController {
-  constructor(private votesMutationService: VotesMutationService) {}
+  constructor(
+    private votesMutationService: VotesMutationService,
+    private votesQueryService: VotesQueryService,
+  ) {}
 
   @Post(':pollId/votes')
   @ApiBearerAuth()
@@ -22,5 +26,27 @@ export class PollVotesController {
       dto.chosenAnswerOptions,
       user,
     );
+  }
+
+  @Get(':pollId/votes/count')
+  @ApiBearerAuth()
+  public getVoteCount(
+    @Param('pollId') pollId: string,
+    // @RequestUser() user: UserEntity,
+  ) {
+    // TODO check user has access to poll
+
+    return this.votesQueryService.countVotes(pollId);
+  }
+
+  @Get(':pollId/votes/percentage')
+  @ApiBearerAuth()
+  public getVotePercentage(
+    @Param('pollId') pollId: string,
+    // @RequestUser() user: UserEntity,
+  ) {
+    // TODO check user has access to poll
+
+    return this.votesQueryService.countVotePercentages(pollId);
   }
 }
