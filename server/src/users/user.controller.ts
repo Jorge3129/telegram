940e66820386cfd42bf924cffb2de0e2279d960d
Heorhii Sanchenko <jorhesancho@gmail.com>
2023-07-16 14:02:12 +0300

    refact(user): BE current user endpoint

--- server/src/users/user.controller.ts
+++ server/src/users/user.controller.ts
@@ -2,12 +2,13 @@ import { Controller, Get } from '@nestjs/common';
 import { User } from './user.type';
 import { RequestUser } from './decorators/user.decorator';
 import { UserEntity } from './entity/user.entity';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Users')
 @Controller('users')
 export class UserController {
-  @Get('/:userId')
+  @Get('/current')
+  @ApiBearerAuth()
   public async getUser(@RequestUser() user: UserEntity): Promise<User> {
     return { ...user, password: '' };
   }
