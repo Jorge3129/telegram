41fd909d99acce3d19044f6295e6d4e8223809f8
Heorhii Sanchenko <jorhesancho@gmail.com>
2023-07-16 15:50:42 +0300

    fix(polls): fix poll vote user relation

--- server/src/polls/entity/poll-vote.entity.ts
+++ server/src/polls/entity/poll-vote.entity.ts
@@ -1,16 +1,16 @@
-import { CreateDateColumn, Entity, ManyToOne, PrimaryColumn } from 'typeorm';
+import { PrimaryColumn, CreateDateColumn, Entity, ManyToOne } from 'typeorm';
 import { PollAnswerOptionEntity } from './poll-answer-option.entity';
 import { UserEntity } from '../../users/entity/user.entity';
 
 @Entity('poll_votes')
 export class PollVoteEntity {
   @PrimaryColumn()
-  userId: string;
+  userId: number;
 
   @PrimaryColumn()
   answerOptionId: string;
 
-  @ManyToOne(() => PollAnswerOptionEntity)
+  @ManyToOne(() => UserEntity)
   user: UserEntity;
 
   @ManyToOne(() => PollAnswerOptionEntity, (answer) => answer.votes)
