47d091611489f2847222f3e630241da4ad559829
Heorhii Sanchenko <jorhesancho@gmail.com>
2023-07-16 10:51:39 +0300

    feat(swagger): setup swagger conditionally on env

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,11 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { setupSwagger } from './swagger';
+import { ConfigService } from '@nestjs/config';
+import { NodeEnv } from './shared/enums/node-env.enum';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  setupSwagger(app);
+  const configService = app.get(ConfigService);
+
+  if (configService.get<string>('NODE_ENV') === NodeEnv.DEV) {
+    setupSwagger(app);
+  }
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
