c1680ed40f060446e2493c42369d8420e660bab6
Heorhii Sanchenko <jorhesancho@gmail.com>
2023-07-23 10:54:39 +0300

    feat(polls): BE add poll text limits

--- server/src/polls/dto/create-poll/create-poll-answer-option.dto.ts
+++ server/src/polls/dto/create-poll/create-poll-answer-option.dto.ts
@@ -1,10 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsBoolean, IsNotEmpty, IsString } from 'class-validator';
+import { IsBoolean, IsNotEmpty, IsString, Length } from 'class-validator';
 
 export class CreatePollAnswerOptionDto {
   @IsString()
   @IsNotEmpty()
   @ApiProperty({ example: "I'm fine, thank you" })
+  @Length(1, 100)
   text: string;
 
   @IsBoolean()
