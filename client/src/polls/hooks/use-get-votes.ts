3130e972a367489a38f206fa380a8b72b39dd76d
Heorhii Sanchenko <jorhesancho@gmail.com>
2023-07-26 20:35:38 +0300

    refactor(polls): FE use lodash keyBy

--- client/src/polls/hooks/use-get-votes.ts
+++ client/src/polls/hooks/use-get-votes.ts
@@ -1,16 +1,19 @@
 import _ from "lodash";
 import { Poll } from "../models/poll.model";
-import { useMemo } from "react";
+import { useCallback, useMemo } from "react";
 
 export const useGetVotes = (poll: Poll) => {
   const groupedVotes = useMemo(
-    () => _.groupBy(poll.answerOptionsWithUsers, "optionId"),
+    () => _.keyBy(poll.answerOptionsWithUsers, "optionId"),
     [poll.answerOptionsWithUsers]
   );
 
-  const getVotes = (optionId: string) => {
-    return groupedVotes[optionId]?.at(0)?.votedUsers || [];
-  };
+  const getVotes = useCallback(
+    (optionId: string) => {
+      return groupedVotes[optionId]?.votedUsers ?? [];
+    },
+    [groupedVotes]
+  );
 
   return getVotes;
 };
